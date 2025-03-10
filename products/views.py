5cfea8decb7e74d7c8ae1e8aa48d3aa340f9c316
Matthew Da Silva <matthewgdasilva25@gmail.com>
2025-03-10 17:39:55 +0000

    add product detail view and template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Item, Tool, PartyItem
 
@@ -31,3 +31,27 @@ def all_products(request):
     }
     
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id): 
+    """
+    A view to show individual product details
+    """
+    
+    product = get_object_or_404(Item, pk=product_id)
+    
+    if product.type == 0:
+        product.image = product.tool.image
+        product.price = product.tool.price
+        product.rating = product.tool.rating
+        product.description = product.tool.description
+    elif product.type == 1:
+        product.image = product.party_item.image
+        product.price = product.party_item.price
+        product.rating = product.party_item.rating
+        product.description = product.party_item.description
+    
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_detail.html', context)
