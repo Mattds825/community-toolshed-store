02f97137fa25630636cb117c591b89898df698b8
Matthew Da Silva <matthewgdasilva25@gmail.com>
2025-03-10 11:19:18 +0000

    build basic layout for products template showing image, name, price and rating

--- products/views.py
+++ products/views.py
@@ -10,9 +10,24 @@ def all_products(request):
     """
     
     products = Item.objects.all()
+    tools = Tool.objects.all()
+    party_items = PartyItem.objects.all()
+    
+    # for each of the products, add the image to the product depending on the type of product
+    for product in products:
+        if product.type == 0:            
+            product.image = product.tool.image
+            product.price = product.tool.price
+            product.rating = product.tool.rating
+        elif product.type == 1:
+            product.image = product.party_item.image
+            product.price = product.party_item.price
+            product.rating = product.party_item.rating
     
     context = {
         'products': products,
+        'tools': tools,
+        'party_items': party_items,
     }
     
     return render(request, 'products/products.html', context)
