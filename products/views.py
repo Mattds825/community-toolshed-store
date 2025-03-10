5970af7e535f665f2ee9b8d83d479fe79b727cfe
Matthew Da Silva <matthewgdasilva25@gmail.com>
2025-03-10 18:14:02 +0000

    add search products functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q, Case, When, CharField, ImageField
 from .models import Item, Tool, PartyItem
 
 # Create your views here.
@@ -10,24 +11,63 @@ def all_products(request):
     """
     
     products = Item.objects.all()
-    tools = Tool.objects.all()
-    party_items = PartyItem.objects.all()
     
-    # for each of the products, add the image to the product depending on the type of product
+    query = None
+    
+    # for each of the products, add the image to the product depending on the type of product        
+    products = Item.objects.all().annotate(
+        description=Case(
+            When(type=0, then='tool__description'),
+            When(type=1, then='partyitem__description'),
+            output_field=CharField(),
+        ),
+        keywords=Case(
+            When(type=0, then='tool__keywords'),
+            When(type=1, then='partyitem__keywords'),
+            output_field=CharField(),
+        ),       
+        price=Case(
+            When(type=0, then='tool__price'),
+            When(type=1, then='partyitem__price'),
+            output_field=CharField(),
+        ),
+        rating=Case(
+            When(type=0, then='tool__rating'),
+            When(type=1, then='partyitem__rating'),
+            output_field=CharField(),
+        )
+    )
+    
+    # quick fix to add image to product
     for product in products:
         if product.type == 0:            
             product.image = product.tool.image
-            product.price = product.tool.price
-            product.rating = product.tool.rating
         elif product.type == 1:
             product.image = product.party_item.image
-            product.price = product.party_item.price
-            product.rating = product.party_item.rating
+    
+    
+    # handle request to filter products by category
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products')) 
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(keywords__icontains=query)
+            products = products.filter(queries)
+            
+            # quick fix to add image to product
+            for product in products:
+                if product.type == 0:            
+                    product.image = product.tool.image
+                elif product.type == 1:
+                    product.image = product.party_item.image
+        
     
     context = {
-        'products': products,
-        'tools': tools,
-        'party_items': party_items,
+        'products': products, 
+        'search_term': query,
     }
     
     return render(request, 'products/products.html', context)
